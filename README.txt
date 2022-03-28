ad88141f53d09bd1ca98ac5ce381eb5046bf629c
saahilanande <saahilanande21@gmail.com>
2022-03-27 22:30:15 -0500

    first

---
+++ README.txt
@@ -0,0 +1,39 @@
+Input:
+
+An input file which would contain one line of input for each reservation request. The order of the lines in the file reflects the order in which the reservation requests were received. Each line in the file will be comprised of a reservation identifier, followed by a space, and then the number of seats requested. The reservation identifier will have the format: R####. Example: R001 2 R002 4 R003 4 R004 3
+
+Output:
+
+The program should output a file containing the seating assignments for each request. Each row in the file should include the reservation number followed by a space, and then a comma-delimited list of the assigned seats. Example: R001 I1,I2 R002 F16,F17,F18,F19 R003 A1,A2,A3,A4 R004 J4,J5,J6
+
+Assumptions made while implementing the Algorithm:
+
+    1.Cost of all the seats in the theatre are same.
+    2.Seats are reserved on the First come first serve basis.
+    3.Customers who reserves the seat first are offered better seats(seats that are far from the screen) than the customers who are reserve later.
+    4.WhenApproach to the implement the solution:
+
+Approach to the implement the solution:
+
+    1.Algorithm uses Greedy approach with Priority Queue implemented using Linked List.
+    2.Each Row is considered as a node and has the following attributes: name, totalSeats, pointer to next node,seatsOccupied and seatsEmpty
+    3.New rows(I,H,G....A) are added when the seats to be reserved are greater than continguous seats empty in the Linked List. If seats in J are smaller than the seats requested a new node with name I is added to the linked list.
+    4.If the row is Full, it is removed from the List and replaced with next row. Example if J is full it was deleted from the tree and I becomes the node.
+    5.Program exits if the all the rows(nodes) are full or cannot accomodate the rest of the reservation with the vacant seats
+
+Program Files:
+
+    inputfile_gen.py: Uses random library to generate random reservation requests.
+    filereader.py: Parse the given input file of above format into list of reservations
+    greedySeatAllocation: Uses greedy approach to allocate seats.
+
+How to run the program:
+
+    Open Command prompt or terminal.
+    Move to the directory where the program is stored. Make sure all the program files are in the same folder before running the program.
+    The program accepts only text files of the format where each line in the file will be comprised of a reservation identifier, followed by a space, and then the number of seats requested. The reservation identifier will have the format: R####. as displayed above.
+    Ensure python module logging is present.
+    Type the following command in the Command Prompt or Terminal. python movietheater.py inputFilePath
+    It takes input file path as system arrgument.
+    The output generated is a text file, Each row in the file should include the reservation number followed by a space, and then a comma-delimited list of the assigned seats.
+    Program also generate the debug logs for event logging.
---
+++ movietheater.py
@@ -0,0 +1,258 @@
+#Author: Saahil Anande
+#This program takes command line input of file path and show the path to the output file with allocated seats to reservation ID.
+
+
+import  logging
+from re import search
+import sys
+from filereader import inputParser
+logging.basicConfig(filename='debug.log',level=logging.DEBUG)
+
+
+
+seats = 20
+output={}
+
+class Node:
+
+    def __init__(self,username,seats,currentNode,seatRequested,reservationID) -> None:
+
+        self.name = username
+        self.seatsOccupied = [0]*seats
+        self.parent = currentNode
+        self.isFull = False
+        self.reserve_seat(seatRequested,reservationID)
+        self.emptySeats = self.check_seat()
+        self.subs=[None,None]
+
+    def reserve_seat(self,seatRequested,reservationID):
+        seatsAssigned=[]
+
+        for seat in range(len(self.seatsOccupied)):
+            if seatRequested != 0:
+                if self.seatsOccupied[seat] == 0:
+                    self.seatsOccupied[seat] = reservationID
+                    seatRequested -= 1
+                    seatsAssigned.append(self.name + str(seat + 1))
+            else:
+                break
+        
+        if reservationID in output:                             ########################
+            output[reservationID]+=","+",".join(seatsAssigned)
+        else:
+            output[reservationID]=",".join(seatsAssigned)
+
+        return self.seatsOccupied
+
+    def check_seat(self):
+        counter=0
+        for i in self.seatsOccupied:
+            if i ==0:
+                counter += 1
+        return counter
+
+
+class Null:
+    def __init__(self,seats) -> None:
+        self.name = None
+        self.seatsPresent = [0]
+        self.emptySeats = None
+        self.subs=[None,None]
+        self.seatsOccupied = [0]*seats
+        self.parent=None
+        self.level = 0
+
+nullNode = Null(seats)
+
+class SeatAssignment:
+
+    def __init__(self) -> None:
+        self.rows = 10
+        self.column = 20
+        self.seatsAvailable = self.column * self.rows
+        self.lastInsert = None
+        self.root = nullNode
+
+    def initallookup(self,seatRequested):
+        if self.root != nullNode:
+            logging.debug("Root node already present")
+            return self.lookup(self.root, seatRequested)
+
+    def lookup(self, currentNode, seatRequested): # recursive function to look for seats with empty seats
+        logging.debug("Current Node is: {} and vacant seat are {} and seat requested are {}".format( currentNode.name,currentNode.emptySeats,seatRequested))
+        if currentNode.emptySeats >= seatRequested:
+            return currentNode
+
+        else:
+            sub = currentNode.subs[1]
+
+            if sub != None:
+                logging.debug("Current Node is {} with vacant seats {}".format(currentNode.name,currentNode.emptySeats))
+                logging.debug("Trying to find better match in other nodes.....")
+                return self.lookup(sub, seatRequested)
+
+            else:
+                logging.debug("No existing vacant node found, creating a new node.....!")
+                return None
+
+
+    def inital_insert(self, seatRequested,reservationID,seats):
+        if self.root != nullNode:
+            self.lastInsert = self.insert(self.root,seatRequested,self.column,reservationID)
+            self.Delete(self.lastInsert)
+
+        else:
+            logging.info("Adding new root row to the List")
+            name = chr(self.column + 64)                    ################
+            self.root = Node(name,seats,None,seatRequested,reservationID)
+            self.column -=1
+            self.lastInsert = self.root
+            self.seatsAvailable = self.seatsAvailable - seatRequested
+
+    def insert(self,currentNode, seatRequested, column, reservationID):
+
+        if seatRequested <= currentNode.emptySeats: #if there are seats avaliable in the row
+            currentNode.seatsOccupied = currentNode.reserve_seat(seatRequested,currentNode.reservationID)
+            currentNode.emptySeats = currentNode.check_seat() #update the empty seats
+            self.seatsAvailable = self.seatsAvailable - seatRequested #update the avaliable seats
+
+        elif seatRequested > currentNode.emptySeats: #if there no seat avaliable in the current node(row), lookup to find best place to add new node
+
+            if currentNode.subs[1]:
+                return self.insert(currentNode.subs[1],seatRequested,column,reservationID) ###########
+
+            elif currentNode.subs[1] == None and column > 0: #adding a new node (row) to the list
+
+                name = chr(self.column + 64)
+                currentNode.subs[1] = Node(name,seats,currentNode,seatRequested,reservationID) #creating a new node
+                logging.info("Addign new row {} to the List".format(name))
+                self.seatsAvailable = self.seatsAvailable - seatRequested #updating avaliable seats
+                self.column -= 1    #subtrating one column
+
+        return currentNode.subs[1]
+
+    def Delete(self,currentNode):   # deleting the nodes(row) from the list for efficient search
+        if currentNode.emptySeats == 0:
+            logging.debug("deleted node {}".format(currentNode.name))
+
+            if currentNode == self.root and currentNode.subs[1] is not None:
+
+                currentNode.subs[1].parent= None
+                self.root=currentNode.subs[1]
+                return self.root
+
+            elif currentNode!= self.root:
+                currentNode.parent.subs[1]=currentNode.subs[1]
+                if currentNode.subs[1]!=None:
+                    currentNode.subs[1].parent=currentNode.parent
+                    return currentNode.parent
+
+            else:
+                self.root=nullNode
+                return self.root
+
+    def verify_seats(self,seatRequested,reservationID): #checking if any back row with seats exists else call insert function
+        logging.debug ("Reservation ID: {} Seat Requested : {} ".format(reservationID,seatRequested))
+        canInsertSeats = False
+
+        if seatRequested > 20:
+            return canInsertSeats
+        
+        node = self.initallookup(seatRequested)
+
+        if node == None and self.column > 0: #no empty seats in the row
+            self.inital_insert(seatRequested,reservationID,seats)
+            canInsertSeats = True
+        
+        elif node != None: #there are avaliable seats
+            logging.debug("No need to add a new node reservation can be accomodated in {}".format(node.name))
+            node.reserve_seat(seatRequested,reservationID)
+            node.emptySeats = node.check_seat()
+            parent = self.Delete(node)
+            self.seatsAvailable = self.seatsAvailable - seatRequested #update the avaliable seats
+            canInsertSeats = True
+
+        elif self.column == 0:
+            return canInsertSeats
+        
+        return canInsertSeats
+
+    def split_insert(self,seatsRequested, reservationID): # spliting the later bookings to utilise the theater
+        logging.info("splitting the booking")
+        currentNode = self.root
+        while currentNode != None and seatsRequested !=0:
+
+            if currentNode.emptySeats <= seatsRequested:
+
+                logging.debug('current node name:{} empty seats:{} current seat requested: {}'.format(currentNode.name,str(currentNode.seatsEmpty),str(seatsRequested)))
+                currentNode.reserve_seat(currentNode.emptySeats,reservationID)
+                seatsRequested -= currentNode.emptySeats        ###########
+                currentNode.emptySeats = currentNode.check_seat()
+                currentNode = currentNode.subs[1]
+
+                if currentNode is not None:
+
+                    self.Delete(currentNode.parent)
+
+            else:
+                logging.debug('current node name {} empty seats:{} seats requested: {}'.format(currentNode.name,str(currentNode.emptySeats),str(seatsRequested)))
+                currentNode.reserve_seat(seatsRequested,reservationID)
+                currentNode.emptySeats = currentNode.check_seat()
+                seatsRequested -= seatsRequested
+                self.Delete(currentNode)
+
+        self.seatsAvailable = self.seatsAvailable - seatsRequested #update the avaliable seats
+
+    def output_writing(self,data,output):
+        logging.info("Program Finished....Writing seats allocated to the file")
+        outfile= open("outfile.txt", 'w+')
+        for eachReservationRequest in data:
+            if eachReservationRequest[0] in output:
+                outfile.write('{} {}\n'.format(eachReservationRequest[0],output[eachReservationRequest[0]]))
+            else:
+                outfile.write('{} {}\n'.format(eachReservationRequest[0], str(0)))
+
+
+
+if __name__ == '__main__':
+    try:
+        filePath =sys.argv[1]
+    except FileNotFoundError as error:
+        print(error)
+
+
+    data = inputParser(filePath)
+
+    seatArrangement = SeatAssignment()
+    not_inserted = []
+
+    for eachReservation in data:
+        if seatArrangement.seatsAvailable == 0:
+            logging.info("Theatre is full no vacant seat available")
+            break
+        
+        if not seatArrangement.verify_seats(eachReservation[1],str(eachReservation[0])):
+            not_inserted.append(eachReservation)
+
+    sorted_not_inserted_bookings=(sorted(not_inserted,key=lambda x:x[1]))
+    more_inserted = []
+
+    for eachReservation in sorted_not_inserted_bookings: # allocating remaining seats by splitting groups to utilize theater
+        if seatArrangement.seatsAvailable == 0:
+            logging.info("Theatre is full no vacant seat available")
+            break
+        elif eachReservation[1] > seatArrangement.seatsAvailable:
+            continue
+        else:
+            more_inserted.append(eachReservation)
+            seatArrangement.split_insert(eachReservation[1], str(eachReservation[0]))
+
+    seatArrangement.output_writing(data,output)
+
+        
+    
+    
+
+
+# newres = Node(20,"saahil",1,3,"112233")
+# temp = SeatAssignment()
